57c613f1eeb3eee38a591ec396b0e30e53926193
Kamaboko0126 <4b232011@gm.student.ncut.edu.tw>
2024-10-01 19:27:40 +0800

    20241001

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,126 @@ export default {
 
 <template>
   <router-view></router-view>
+
+  <!-- <header>
+    <div class="header-container">
+      <h1 :style="{ color: logoColor }">Seaport Handicrafts</h1>
+      <h1>Seaport Handicrafts</h1>
+      <h6 :style="{ color: logoColor }">
+        - Contemporary Knot Art of Hsu Pei-Tzu -
+      </h6>
+    </div>
+  </header> -->
+  <!-- <section class="first">
+    <div class="bg first">
+      <div class="left"></div>
+
+      <div class="right" id="right">
+        <div class="text-content">
+          <p>
+            人說：「女人四十一枝花，我八十初頭了」，取名為「二蕊花阿嬤藝術家」；學藝術的職業婦女相當艱辛，在傳統父權至上的家庭更為如此，因此有一股不服輸的精神，所以毅然走上……藝術創作之路。<br /><br />
+            在學習歷程中，跟著廖明依老師學習，從工筆花鳥到膠彩畫。幸運的我，當年畫作參加兩次中部美展皆得獎，台中縣美展連續三年獲得前三名，因此成為永久免審藝術家，每年都受邀中縣美展，縣市合併後就受邀參加台中市大墩文化中心的女性藝術家聯展。<br /><br />
+            爾後每年臺中市女性藝術家聯展、松柏國民美展，就成為我生命中最值得分享的經驗。之後又接觸結藝編織，是受到娘家姪女(徐秀鑾)的鼓舞；剛開始做一些小鑰匙圈與人結緣，朋友們看到覺得很精緻，鼓勵我拿去參展。近年來與膠彩畫的玉美、瑞瓊、真真、佳以、文珍、秀梅，等同事朋友共組124畫會研究創作，相互學習鼓勵，開啟女性藝術家藝術文化薪傳的秘密基地—中央路女性藝術家畫室。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <!-- <section class="second-for-phone">
+    <div class="outer">
+      <div class="inner">
+        <div class="bg second-for-phone">
+          <div class="text-content">
+            <p>
+              人說：「女人四十一枝花，我八十初頭了」，取名為「二蕊花阿嬤藝術家」；學藝術的職業婦女相當艱辛，在傳統父權至上的家庭更為如此，因此有一股不服輸的精神，所以毅然走上……藝術創作之路。<br /><br />
+              在學習歷程中，跟著廖明依老師學習，從工筆花鳥到膠彩畫。幸運的我，當年畫作參加兩次中部美展皆得獎，台中縣美展連續三年獲得前三名，因此成為永久免審藝術家，每年都受邀中縣美展，縣市合併後就受邀參加台中市大墩文化中心的女性藝術家聯展。<br /><br />
+              爾後每年臺中市女性藝術家聯展、松柏國民美展，就成為我生命中最值得分享的經驗。之後又接觸結藝編織，是受到娘家姪女(徐秀鑾)的鼓舞；剛開始做一些小鑰匙圈與人結緣，朋友們看到覺得很精緻，鼓勵我拿去參展。近年來與膠彩畫的玉美、瑞瓊、真真、佳以、文珍、秀梅，等同事朋友共組124畫會研究創作，相互學習鼓勵，開啟女性藝術家藝術文化薪傳的秘密基地—中央路女性藝術家畫室。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="second">
+    <div class="outer">
+      <div class="inner">
+        <div class="bg second">
+          <div class="left">
+            <div class="top">
+              <h1>徐碧姿</h1>
+              <h3>Hsu Pei-Tzu</h3>
+            </div>
+            <div class="bottom">
+              <div class="text-content">
+                <p>
+                  徐老師的編織結藝最與眾不同的是，她將膠彩畫的發光礦物顏料與多色顏料結合於編織作品中，用她敏銳獨到的觀察力，從生活中的花草、蟲、魚、蝶、蔬果等觀察，尋找最貼近生活的題材，在日常的生活中尋找俯拾即是的美感，並運用其巧手編織將所見轉化爲更具美與意的創作，並藉此表達對自然的關懷。<br />
+                  (文/柯丁祺)
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="right">
+            <div class="background"></div>
+            <div
+              class="img"
+              :style="{ 'background-image': `url(${artistImg})` }"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="third">
+    <div class="outer">
+      <div class="inner">
+        <div class="bg third">
+          <div class="header-block"></div>
+          <div class="container">
+            <div class="imgs">
+              <img v-for="img in artworkImgs" :key="img" :src="img" />
+            </div>
+          </div>
+          <div class="bottom-block"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="fourth">
+    <div class="outer">
+      <div class="inner">
+        <div class="bg fourth">
+          <h1>這是footer，放一些展覽的資訊，如:時間、地點等</h1>
+        </div>
+      </div>
+    </div>
+  </section> -->
 </template>
 
 <style lang="scss">
 :root {
-  --logo-padding: 60px;
+  --logo-padding: 50px;
   --banner-left-width: 430px;
   --main-font-size: 19px;
-  --h1-size: 35px;
-  --h6-size: 34px;
+  --h1-size: 30px;
+  --h6-size: 30px;
   --line-height: 2;
 
-  @media (max-width: 1024px) {
-    --logo-padding: 60px;
-    --banner-left-width: 430px;
-    --main-font-size: 19px;
-    --h1-size: 35px;
-    --h6-size: 34px;
-    --line-height: 2;
-  }
+  // @media (max-width: 1024px) {
+  //   --logo-padding: 60px;
+  //   --banner-left-width: 430px;
+  //   --main-font-size: 19px;
+  //   --h1-size: 35px;
+  //   --h6-size: 34px;
+  //   --line-height: 2;
+  // }
 
   @media (max-width: 768px) {
-    --logo-padding: 60px;
-    --banner-left-width: 430px;
-    --main-font-size: 19px;
-    --h1-size: 35px;
-    --h6-size: 34px;
-    --line-height: 2;
+  --logo-padding: 50px;
+  --banner-left-width: 430px;
+  --main-font-size: 14px;
+  --h1-size: 20px;
+  --h6-size: 20px;
+  --line-height: 1.7;
   }
 }
 * {
--- src/pages/HomePage(v3).vue
+++ src/pages/HomePage(v3).vue
@@ -84,18 +84,28 @@ export default {
 </script>
 
 <template>
+  <div class="loader">
+    <div class="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <header>
+    <h1>Seaport Handicrafts</h1>
+    <h6>- Contemporary Knot Art of Hsu Pei-Tzu -</h6>
+  </header>
   <div class="banner-animation">
     <div class="imgs">
       <img v-for="i in 5" :key="i" :src="bannerImg" class="img" />
     </div>
   </div>
   <section class="banner-container">
-    .
-    <div class="top">
-      <h1>Seaport Handicrafts</h1>
-      <h1>Seaport Handicrafts</h1>
-      <h6>- Contemporary Knot Art of Hsu Pei-Tzu -</h6>
-    </div>
     <div class="bottom">
       <img :src="iconImg" />
     </div>
@@ -103,154 +113,52 @@ export default {
 
   <main>
     <section class="first">
-
+      <div class="text-content">
+        <p>
+          　　人說：「女人四十一枝花，我八十初頭了」，取名為「二蕊花阿嬤藝術家」；學藝術的職業婦女相當艱辛，在傳統父權至上的家庭更為如此，因此有一股不服輸的精神，所以毅然走上……藝術創作之路。<br /><br />
+          　　在學習歷程中，跟著廖明依老師學習，從工筆花鳥到膠彩畫。幸運的我，當年畫作參加兩次中部美展皆得獎，台中縣美展連續三年獲得前三名，因此成為永久免審藝術家，每年都受邀中縣美展，縣市合併後就受邀參加台中市大墩文化中心的女性藝術家聯展。<br /><br />
+          　　爾後每年臺中市女性藝術家聯展、松柏國民美展，就成為我生命中最值得分享的經驗。之後又接觸結藝編織，是受到娘家姪女(徐秀鑾)的鼓舞；剛開始做一些小鑰匙圈與人結緣，朋友們看到覺得很精緻，鼓勵我拿去參展。近年來與膠彩畫的玉美、瑞瓊、真真、佳以、文珍、秀梅，等同事朋友共組124畫會研究創作，相互學習鼓勵，開啟女性藝術家藝術文化薪傳的秘密基地—中央路女性藝術家畫室。
+        </p>
+      </div>
     </section>
+    <section class="second"></section>
+    <section class="third"></section>
   </main>
-
-  <!-- <header>
-    <div class="header-container">
-      <h1 :style="{ color: logoColor }">Seaport Handicrafts</h1>
-      <h1>Seaport Handicrafts</h1>
-      <h6 :style="{ color: logoColor }">
-        - Contemporary Knot Art of Hsu Pei-Tzu -
-      </h6>
-    </div>
-  </header> -->
-  <!-- <section class="first">
-    <div class="bg first">
-      <div class="left"></div>
-
-      <div class="right" id="right">
-        <div class="text-content">
-          <p>
-            　　人說：「女人四十一枝花，我八十初頭了」，取名為「二蕊花阿嬤藝術家」；學藝術的職業婦女相當艱辛，在傳統父權至上的家庭更為如此，因此有一股不服輸的精神，所以毅然走上……藝術創作之路。<br /><br />
-            　　在學習歷程中，跟著廖明依老師學習，從工筆花鳥到膠彩畫。幸運的我，當年畫作參加兩次中部美展皆得獎，台中縣美展連續三年獲得前三名，因此成為永久免審藝術家，每年都受邀中縣美展，縣市合併後就受邀參加台中市大墩文化中心的女性藝術家聯展。<br /><br />
-            　　爾後每年臺中市女性藝術家聯展、松柏國民美展，就成為我生命中最值得分享的經驗。之後又接觸結藝編織，是受到娘家姪女(徐秀鑾)的鼓舞；剛開始做一些小鑰匙圈與人結緣，朋友們看到覺得很精緻，鼓勵我拿去參展。近年來與膠彩畫的玉美、瑞瓊、真真、佳以、文珍、秀梅，等同事朋友共組124畫會研究創作，相互學習鼓勵，開啟女性藝術家藝術文化薪傳的秘密基地—中央路女性藝術家畫室。
-          </p>
-        </div>
-      </div>
-    </div>
-  </section> -->
-  <!-- <section class="second-for-phone">
-    <div class="outer">
-      <div class="inner">
-        <div class="bg second-for-phone">
-          <div class="text-content">
-            <p>
-              　　人說：「女人四十一枝花，我八十初頭了」，取名為「二蕊花阿嬤藝術家」；學藝術的職業婦女相當艱辛，在傳統父權至上的家庭更為如此，因此有一股不服輸的精神，所以毅然走上……藝術創作之路。<br /><br />
-              　　在學習歷程中，跟著廖明依老師學習，從工筆花鳥到膠彩畫。幸運的我，當年畫作參加兩次中部美展皆得獎，台中縣美展連續三年獲得前三名，因此成為永久免審藝術家，每年都受邀中縣美展，縣市合併後就受邀參加台中市大墩文化中心的女性藝術家聯展。<br /><br />
-              　　爾後每年臺中市女性藝術家聯展、松柏國民美展，就成為我生命中最值得分享的經驗。之後又接觸結藝編織，是受到娘家姪女(徐秀鑾)的鼓舞；剛開始做一些小鑰匙圈與人結緣，朋友們看到覺得很精緻，鼓勵我拿去參展。近年來與膠彩畫的玉美、瑞瓊、真真、佳以、文珍、秀梅，等同事朋友共組124畫會研究創作，相互學習鼓勵，開啟女性藝術家藝術文化薪傳的秘密基地—中央路女性藝術家畫室。
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="second">
-    <div class="outer">
-      <div class="inner">
-        <div class="bg second">
-          <div class="left">
-            <div class="top">
-              <h1>徐碧姿</h1>
-              <h3>Hsu Pei-Tzu</h3>
-            </div>
-            <div class="bottom">
-              <div class="text-content">
-                <p>
-                  徐老師的編織結藝最與眾不同的是，她將膠彩畫的發光礦物顏料與多色顏料結合於編織作品中，用她敏銳獨到的觀察力，從生活中的花草、蟲、魚、蝶、蔬果等觀察，尋找最貼近生活的題材，在日常的生活中尋找俯拾即是的美感，並運用其巧手編織將所見轉化爲更具美與意的創作，並藉此表達對自然的關懷。<br />
-                  (文/柯丁祺)
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="right">
-            <div class="background"></div>
-            <div
-              class="img"
-              :style="{ 'background-image': `url(${artistImg})` }"
-            ></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="third">
-    <div class="outer">
-      <div class="inner">
-        <div class="bg third">
-          <div class="header-block"></div>
-          <div class="container">
-            <div class="imgs">
-              <img v-for="img in artworkImgs" :key="img" :src="img" />
-            </div>
-          </div>
-          <div class="bottom-block"></div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="fourth">
-    <div class="outer">
-      <div class="inner">
-        <div class="bg fourth">
-          <h1>這是footer，放一些展覽的資訊，如:時間、地點等</h1>
-        </div>
-      </div>
-    </div>
-  </section> -->
 </template>
 
 <style scoped lang="scss">
-.banner-container {
-  height: 100vh;
+* {
+  box-sizing: border-box;
+  user-select: none;
+}
+
+header {
+  height: 150px;
   width: 100%;
-  position: relative;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  position: fixed;
+  z-index: 999;
   top: 0;
   left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-  .top {
-    width: 100%;
-    height: 60%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    padding: var(--logo-padding) 0 0 var(--logo-padding);
-    h1 {
-      text-transform: uppercase;
-      font-family: "Playfair Display";
-      line-height: normal;
-      font-weight: 500;
-      color: #fff;
-      transition: 1s;
-      &:nth-child(2) {
-        color: #f8bc6e;
-      }
-    }
-    h6 {
-      font-family: "Qwigley";
-      font-weight: 400;
-      transition: 1s;
-      color: #fff;
-      transition: 1s;
-    }
+  // background: #232323;
+  padding: 0 3vw;
+  h1 {
+    text-transform: uppercase;
+    font-family: "Playfair Display";
+    line-height: normal;
+    font-weight: 500;
+    color: #f8bc6e;
   }
-  .bottom {
-    width: 100%;
-    height: 40%;
-    padding: 0 0 var(--logo-padding) var(--logo-padding);
-    display: flex;
-    align-items: flex-end;
-    justify-content: flex-start;
-    img {
-      height: 100%;
-    }
+  h6 {
+    font-family: "Qwigley";
+    font-weight: 400;
+    color: #fff;
   }
 }
+
 .banner-animation {
   width: 100%;
   height: 100vh;
@@ -265,7 +173,6 @@ export default {
     img {
       height: 100vh;
       margin-left: -1px;
-
       &:nth-child(2n) {
         transform: scaleX(-1);
       }
@@ -273,20 +180,129 @@ export default {
   }
 }
 
-* {
-  box-sizing: border-box;
-  user-select: none;
-}
-
-.section {
-  height: 300vh;
+.banner-container {
+  height: 100vh;
   width: 100%;
   position: relative;
   top: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  .bottom {
+    width: 100%;
+    height: 100%;
+    padding: 0 0 7vh 7vw;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+    @media (max-width: 768px) {
+      padding: 0 0 4vh 9vw;
+    }
+    img {
+      height: 50%;
+      @media (max-width: 768px) {
+        height: 40%;
+      }
+    }
+  }
 }
 
-.first{
+main {
+  height: 1000vh;
+}
+
+.first {
+  background: #ffffff95;
+  clip-path: polygon(0 10%, 100% 0, 100% 100%, 0 100%);
+  padding: 10vh 5vw 5vh 5vw;
+
+  @media (max-width: 768px) {
+    clip-path: polygon(0 3%, 100% 0, 100% 100%, 0 100%);
+    padding: 6vh 8vw 5vh 8vw;
+  }
+}
+.loader {
+  position: fixed;
+  z-index: 99999;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background: #fff;
+  display: none;
+}
+
+.spinner {
+  --radius: 100;
+  --size: 50;
+  position: relative;
+  width: calc(var(--size) * 1px);
+  height: calc(var(--size) * 1px);
+  div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #474bff;
+    border-radius: 50%;
+    transform: rotate(calc(var(--angle) * 1deg))
+      translate(0, calc(var(--radius) * 0px));
+    animation: spinner-19rk4d 1.5s calc(var(--delay) * 1.2s) infinite ease;
+    &:nth-child(1) {
+      --angle: 45;
+      --delay: 0.1;
+    }
+    &:nth-child(2) {
+      --angle: 90;
+      --delay: 0.2;
+    }
+    &:nth-child(3) {
+      --angle: 135;
+      --delay: 0.3;
+    }
+
+    &:nth-child(4) {
+      --angle: 180;
+      --delay: 0.4;
+    }
+
+    &:nth-child(5) {
+      --angle: 225;
+      --delay: 0.5;
+    }
+
+    &:nth-child(6) {
+      --angle: 270;
+      --delay: 0.6;
+    }
+
+    &:nth-child(7) {
+      --angle: 315;
+      --delay: 0.7;
+    }
+
+    &:nth-child(8) {
+      --angle: 360;
+      --delay: 0.8;
+    }
+  }
+}
+@keyframes spinner-19rk4d {
+  0%,
+  30%,
+  50%,
+  100% {
+    transform: rotate(calc(var(--angle) * 1deg))
+      translate(0, calc(var(--radius) * 0px)) scale(0);
+  }
+
+  40% {
+    transform: rotate(calc(var(--angle) * 1deg))
+      translate(0, calc(var(--radius) * 1px)) scale(1);
+  }
 }
 </style>
